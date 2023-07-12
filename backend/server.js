cc1ad3da9aa20b1664e9522611e2c46d8fce391c
sauravdevp <saurav.kumar@brightcodess.com>
2023-07-12 16:58:39 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express=require("express");
+const app=express();
+const cors=require('cors');
+const mysql=require('mysql');
+
+app.use(cors());
+
+app.use(express.json());
+
+const db=mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'db_crud'
+});
+db.connect(function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log('connection succsful')
+    }
+});
+
+app.get('/',(req,res) => {
+    res.json('hlo first')
+});
+
+app.get('/all_students/', (req,res) => {
+    const sql='select * from student';
+    db.query(sql,(err,data) => {
+        if(err){
+            return res.json('Error');
+        }else{
+            return res.json(data)
+        }
+    });
+});
+
+
+app.listen(8081, () => {
+    console.log('hi');
+});
