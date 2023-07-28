8fccbb1616ba4551dac00411cd8492c052a664b5
sauravdevp <saurav.kumar@brightcodess.com>
2023-07-28 16:39:41 +0530

    ss

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 app.use(express.json());
 
-const upload=multer({dest:'uploads/'});
+// const upload=multer({dest:'uploads/'});
 
 app.use(express.urlencoded({extended: false}));
 
@@ -29,16 +29,57 @@ db.connect(function(err){
 
 const storage=multer.diskStorage({
     destination: function(req,file,cb){
-        return cb(null, "/uploads");
+        return cb(null, "uploads");
     },
     filename: function(req, file, cb){
         return cb(null,`${Date.now()}-${file.originalname}`);
     },
 });
 
-const uploads=multer({
-    storage: storage
-}).single('doc');
+const checkFileType = function (file, cb) {
+    //Allowed file extensions
+    const fileTypes = /jpeg|jpg|png|gif|svg/;
+  
+    //check extension names
+    const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  
+    const mimeType = fileTypes.test(file.mimetype);
+  
+    if (mimeType && extName) {
+      return cb(null, true);
+    } else {
+      cb("Error: You can Only Upload Images!!");
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 10000000 },
+    fileFilter: (req, file, cb) => {
+        checkFileType(file, cb);
+    },
+});
+
+app.post("/single", upload.single("image"), (req, res) => {
+    if (req.file) {
+        values=[
+            req.file.path,
+            req.body.name
+        ]
+        res.send(values);
+    //   res.send("Single file uploaded successfully");
+    } else {
+      res.status(400).send("Please upload a valid image");
+    }
+});
+app.post("/multiple_image", upload.array("images",5), (req, res) => {
+    if (req.files) {
+        res.send(req.files.path);
+    //   res.send("Single file uploaded successfully");
+    } else {
+      res.status(400).send("Please upload a valid image");
+    }
+});
 
 app.get('/',(req,res) => {
     res.json('hlo first');
@@ -55,12 +96,12 @@ app.get('/all_students', (req,res) => {
     });
 });
 
-app.post('/create', uploads, (req,res) => {
-    const sql='insert into student (`name`,`email`,`user_file`) values(?)';
+app.post('/create', (req,res) => {
+    const sql='insert into student (`name`,`email`) values(?)';
     const values=[
         req.body.name,
         req.body.email,
-        req.files.doc,
+        // req.files.doc,
     ]
     db.query(sql,[values], (err,data) => {
         if(err){
@@ -104,17 +145,21 @@ app.delete('/delete/:id', (req,res) => {
 
 app.get('/single_students/:id', (req,res) => {
     const sql='select * from student where id=?';
-    const id=req.params.id
+    const id=req.params.id;
     db.query(sql,[id],(err,data) => {
         if(err){
             return res.json('Error');
         }else{
-            return res.json(data)
+            return res.json(data);
         }
     });
 });
 
+const port = process.env.PORT || 8081;
 
-app.listen(8081, () => {
-    console.log('hi');
+app.listen(port, ()=>{
+    console.log(`App is listening on port ${port}`);
 });
+// app.listen(8081, () => {
+//     console.log('run on port 8081');
+// });
